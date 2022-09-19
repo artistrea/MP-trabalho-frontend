dcf47070a6db4a926688d1e21b38eeef565c8cb8
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-19 16:47:12 -0300

    better create and edit product page

--- src/components/ProductForm/index.jsx
+++ src/components/ProductForm/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import apiCreateProduct from "../../api/requests/products/create";
 import apiUpdateProduct from "../../api/requests/products/update";
 import Input from "../Input";
@@ -6,14 +7,16 @@ import Input from "../Input";
 export const ProductForm = ({ startingProduct, update }) => {
     const [product, setProduct] = useState(startingProduct || {});
 
+    const navigate = useNavigate()
+    
     const handleCreateProduct = () => {
         apiCreateProduct(product)
-            .then(() => alert("Criado com sucesso"))
+            .then(() => {alert("Criado com sucesso"); navigate("/admin/products")})
             .catch(alert)
     };
     const handleUpdateProduct = () => {
         apiUpdateProduct(product)
-            .then(() => alert("Editado com sucesso"))
+            .then(() => {alert("Editado com sucesso"); navigate("/admin/products")})
             .catch(alert)
     };
 
@@ -28,10 +31,15 @@ export const ProductForm = ({ startingProduct, update }) => {
     }
 
     return <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nome do produto</label>
         <Input id="name" placeholder="Nome" onChange={handleChange} value={product.name} />
+        <label htmlFor="name">Descrição</label>
         <textarea rows={5} id="description" placeholder="Descrição" onChange={handleChange} value={product.description} />
+        <label htmlFor="name">Estoque</label>
         <Input id="quantity_in_stock" placeholder="Quantidade em estoque" onChange={handleChange} value={product.quantity_in_stock} />
+        <label htmlFor="name">Tempo de preparo (em minutos)</label>
         <Input id="prep_time_in_minutes" placeholder="Tempo médio de preparar" onChange={handleChange} value={product.prep_time_in_minutes} />
+        <label htmlFor="name">Preço (em centavos)</label>
         <Input id="price_in_cents" placeholder="Preço em centavos" onChange={handleChange} value={product.price_in_cents} />
         <button type="submit" style={{marginInline: "auto"}}>
             {update ? "Editar" : "Criar"}
