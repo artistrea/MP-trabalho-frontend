49affdbbec5e2670a81b7f17e44ab47c80c05a1f
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-19 13:23:20 -0300

    contact page working

--- src/api/requests/contact_us.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "..";
-import { messageInfo } from "../validations/messageInfo";
-
-export default async function contact_us(messageInfo: messageInfo) {
-  return await api.post("contact_us", { message: messageInfo });
-}
--- src/pages/ContactPage/index.tsx
+++ src/pages/ContactPage/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEventHandler, FC, FormEventHandler, useState } from "react";
 import { z } from "zod";
 import dealWithZodError from "../../api/utils/dealWithZodError";
 import sendContactMessage from "../../api/utils/sendContactMessage";
+import useMessagesIndex from "../../api/utils/useContactUsIndex";
 import Input from "../../components/Input";
 import "./style.scss";
 
@@ -11,6 +12,8 @@ export const ContactPage: FC = () => {
         name: "",
         message: "",
     });
+
+    const messages = useMessagesIndex();
 
     const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
         setMessageInfo({ ...messageInfo, [e.target.id]: e.target.value });
@@ -35,9 +38,13 @@ export const ContactPage: FC = () => {
 
     return (
         <div className="flex container">
-            <img src="https://images.unsplash.com/photo-1601599967100-f16100982063?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJvemVuJTIwZm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+            <img
+                className="contact-page__img"
+                src="https://images.unsplash.com/photo-1601599967100-f16100982063?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJvemVuJTIwZm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            />
             <section>
                 <h1>Fale conosco</h1>
+                <p>Coloque aqui sua reclamação, sugestão ou avaliação</p>
                 <form onSubmit={handleSubmit}>
                     <div className="top-inputs">
                         <Input
@@ -64,6 +71,31 @@ export const ContactPage: FC = () => {
                     />
                     <button type="submit">Enviar</button>
                 </form>
+                {messages && <h2>Mensagens:</h2>}
+                <ul
+                    style={{
+                        listStyle: "none",
+                        width: "100%",
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: "3rem",
+                    }}
+                >
+                    {messages?.map((m) => (
+                        <li style={{ marginBlock: "2rem" }}>
+                            <h3
+                                style={{
+                                    fontWeight: 500,
+                                }}
+                            >
+                                Por: {m.name || "Anônimo"}
+                            </h3>
+                            <small>{m.email}</small> <br />
+                            Mensagem: <br />
+                            <textarea disabled value={m.message}></textarea>
+                        </li>
+                    ))}
+                </ul>
             </section>
         </div>
     );
